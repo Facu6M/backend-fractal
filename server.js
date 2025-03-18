7eee8f8766f22c54566c1be6ab502c4cd27ca499
Facu6M <facu6p@gmail.com>
2025-03-18 19:44:13 -0300

    add cors

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 let orders = [
   {
     id: 1,
